38b13c330dd2c0ea3ccddc47cb9ca01c3272acb4
Алексей <97665299+0ne290@users.noreply.github.com>
2025-01-10 23:50:38 +0800

    Update entities.go

--- internal/core/domain/entities.go
+++ internal/core/domain/entities.go
@@ -8,17 +8,17 @@ import (
 
 type Wallet struct {
 	Uuid []byte
-	// Сумма на балансе в наименьших единицах. Если счет подразумевается, например, долларовый, то сумма на балансе будет измеряться в центах и тогда значение 7950 будет эквивалентно $79.5.
-	Balance uint64
+	// Сумма на балансе в наименьших единицах. Если счет подразумевается, например, долларовый, то сумма на балансе будет измеряться в центах и тогда значение 7950 будет эквивалентно $79.5. Изначально я хотел использовать uint64, чтобы увеличить верхнюю границу допустимого диапазона на X2 + 1, но у PostgreSQL проблемы с беззнаковыми типами.
+	Balance int64
 }
 
 func NewWallet(uuidProvider UuidProvider) *Wallet {
 	return &Wallet{uuidProvider.Random(), 0}
 }
 
-func (wallet *Wallet) Deposit(amount uint64) error {
-	if math.MaxUint64 - wallet.Balance < amount {
-	    return errors.New(fmt.Sprint("balance cannot be greater than ", uint64(math.MaxUint64)))
+func (wallet *Wallet) Deposit(amount int64) error {
+	if math.MaxInt64 - wallet.Balance < amount {
+	    return errors.New(fmt.Sprint("balance cannot be greater than ", math.MaxInt64))
 	}
 	
 	wallet.Balance += amount
@@ -26,7 +26,7 @@ func (wallet *Wallet) Deposit(amount uint64) error {
 	return nil
 }
 
-func (wallet *Wallet) Withdraw(amount uint64) {
+func (wallet *Wallet) Withdraw(amount int64) {
 	if amount > wallet.Balance
 	    return errors.New("balance cannot be less than 0")
 	
